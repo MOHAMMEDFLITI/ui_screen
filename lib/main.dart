b835fae627625c531a371224f5172c585c0eb85b
Mohammed Fliti <mohammedfliti02@gmail.com>
2022-11-27 10:59:00 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:figma_to_flutter/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: const Color(0xff29274F),
+      ),
+      home:HomeScreen(),
+    );
+  }
+}
