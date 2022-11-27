8d7ad969848c7b04010325c1f7c51f2694d6fdc1
Mohammed Fliti <mohammedfliti02@gmail.com>
2022-11-27 17:34:45 +0100

    card commit

---
+++ lib/components/components.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+horizonteList({
+  required String firstCourseImage,
+  String? secondCourseImage,
+  required String courseHeadLine,
+  required String firstCourseTitle,
+  String? secondCourseTitle,
+  required int startColor,
+  required int endColor,
+}) {
+  return Padding(
+    padding: const EdgeInsets.only(
+      right: 26,
+    ),
+    child: Container(
+      height: 349,
+      width: 246,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(22),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: const Alignment(1, 0),
+          colors: [
+            Color(startColor),
+            Color(startColor),
+          ],
+        ),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            top: 15,
+            left: 11,
+            child: Container(
+              height: 39,
+              width: 115,
+              decoration: BoxDecoration(
+                color: const Color(0xFFAFA8EE),
+                borderRadius: BorderRadius.circular(36),
+              ),
+              child: Center(
+                child: Text(
+                  courseHeadLine,
+                  style: GoogleFonts.roboto(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+
+            ),
+          ),
+          Positioned(
+            top: 26+15+39,
+            left: 14,
+            child: Text(
+              firstCourseTitle,
+              style: GoogleFonts.roboto(
+                color:Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 31+80,
+            left: 14,
+            child: Text(
+              secondCourseTitle!,
+              style: GoogleFonts.roboto(
+                color:Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+              ),
+            ),
+          ),
+          Positioned(
+            top:192,
+            left:100,
+            child:SvgPicture.asset(
+              secondCourseImage!,
+            ),
+          ),
+          Positioned(
+            top:70,
+            left:37,
+            child:Image.asset(
+                firstCourseImage,
+            ),
+          ),
+
+        ],
+      ),
+    ),
+  );
+}
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:figma_to_flutter/components/components.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -8,50 +9,68 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       //backgroundColor: Theme.of(context).backgroundColor,
       body: SafeArea(
-          child:ListView(
-            physics: const BouncingScrollPhysics(),
-            padding: const EdgeInsets.only(
-              left: 20,
-              right: 20,
-            ),
-            children:
-            [
-              Padding(
-                padding: const EdgeInsets.only(
-                  top:55,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:
-                  [
-                    Text(
-                      'Online',
-                      style:GoogleFonts.roboto(
-                        fontSize: 36,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 11,
-                    ),
-                    Text(
-                      'Master Class',
-                      style:GoogleFonts.roboto(
-                        fontSize: 36,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 22,
-              ),
-            ],
+        child: ListView(
+          physics: const BouncingScrollPhysics(),
+          padding: const EdgeInsets.only(
+            left: 20,
+            right: 20,
           ),
+          children:
+          [
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 55,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children:
+                [
+                  Text(
+                    'Online',
+                    style: GoogleFonts.roboto(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 11,
+                  ),
+                  Text(
+                    'Master Class',
+                    style: GoogleFonts.roboto(
+                      fontSize: 36,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 22,
+            ),
+            SizedBox(
+              height: 349,
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) =>horizonteList(
+                  startColor: 0xFF9288E4,
+                  endColor: 0xFF534EA7,
+                  firstCourseTitle: 'UI/UX DESIGNER',
+                  secondCourseTitle: 'BEGINNER',
+                  courseHeadLine: 'Recommended',
+                  firstCourseImage: 'assets/images/1.png',
+                  secondCourseImage: 'assets/images/2.svg',
+                ),
+                itemCount: 3,
+
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
