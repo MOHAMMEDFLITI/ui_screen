1a43d4a39a29a0f3ecefa52d1d8ab87b936a5b82
Mohammed Fliti <mohammedfliti02@gmail.com>
2022-11-27 13:33:14 +0100

    text

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //var size = MediaQuery.of(context).size;
     return Scaffold(
       //backgroundColor: Theme.of(context).backgroundColor,
-      appBar: AppBar(),
-      body: null,
+      body: SafeArea(
+          child:ListView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.only(
+              left: 20,
+              right: 20,
+            ),
+            children:
+            [
+              Padding(
+                padding: const EdgeInsets.only(
+                  top:55,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children:
+                  [
+                    Text(
+                      'Online',
+                      style:GoogleFonts.roboto(
+                        fontSize: 36,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 11,
+                    ),
+                    Text(
+                      'Master Class',
+                      style:GoogleFonts.roboto(
+                        fontSize: 36,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 22,
+              ),
+            ],
+          ),
+      ),
     );
   }
 }
